fd1ed0df4e46880ef7aa50044407fd6af95aabe6
Lewis Nozaki <lhnozaki@gmail.com>
2019-06-23 18:03:33 -1000

    added modal and selections

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,1 +1,22 @@
-console.log("Test 1");
+let testBtn = document.querySelector("#selectOne");
+let modal = document.querySelector("#modal");
+
+
+testBtn.addEventListener("change", function() {
+  let slider = document.querySelectorAll(".slider")[0];
+  if(this.value === "Select-Color") {
+    slider.style.display = "none";
+    console.log("Please choose a color");
+  } else if(this.value == "Green") {
+    console.log("You chose Green")
+  } else if(this.value == "Pink") {
+    modal.style.display = "flex";
+  } else if(this.value == "White") {
+    console.log("You chose White");
+  }
+})
+
+let closeBox = document.querySelector("#closeBox");
+closeBox.addEventListener("click", function() {
+  modal.style.display = "none";
+});
