a7ce446da65928aa094260367cb304ecf03cb85e
Lewis Nozaki <lhnozaki@gmail.com>
2019-06-23 23:45:08 -1000

    start building the register UI

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,8 +1,20 @@
-/// Declare Variables
+/// Declare UI Variables
 let testBtn = document.querySelector("#selectOne");
 let modal = document.querySelector("#modal");
 let closeBox = document.querySelector("#closeBox");
 let shoe = document.querySelectorAll(".shoe");
+let cart = document.querySelector("#cart");
+let subtotal = document.querySelector("#Subtotal");
+let quantity = document.querySelector("#quantity");
+let price = 100.00;
+let tax = document.querySelector("#Tax");
+let total = document.querySelector("#Total");
+let startBalance = 300;
+let blBtn = document.querySelector("#BL");
+let balance = document.querySelector("#Balance");
+let clear = document.querySelector("#CL");
+let calcInputScreen = document.querySelector("#calcInputs");
+let numbers = document.querySelectorAll(".number");
 
 /// Modal Functions
 testBtn.addEventListener("change", function() {
@@ -40,3 +52,40 @@ testBtn.addEventListener("change", function() {
 closeBox.addEventListener("click", function() {
   modal.style.display = "none";
 });
+
+/// Add to cart button
+cart.addEventListener("click", function() {
+  let newSubtotal = price * quantity.value;
+  subtotal.value = `$${newSubtotal}`;
+  let newTax = newSubtotal * .04;
+  tax.value = `$${newTax}`;
+  let newTotal = newSubtotal + newTax;
+  total.value = `$${newTotal}`;
+})
+
+/// Balance button
+blBtn.addEventListener("click", function() {
+  balance.value = `$${startBalance}`;
+  if(startBalance > 0){
+    balance.style.color = "aqua";
+  } else {
+    balance.style.color = "red";
+  }
+})
+
+/// Clear button
+clear.addEventListener("click", function() {
+  subtotal.value = "";
+  tax.value = "";
+  total.value = "";
+  balance.value = "";
+  calcInputScreen.value = "";
+})
+
+
+numbers.forEach(function(e) {
+  e.addEventListener("click", function() {
+    calcInputScreen.value += this.textContent;
+    })
+})
+
